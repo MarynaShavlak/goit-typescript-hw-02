15b8d7a4d91f1c612ca41b84087c67799f022b28
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-02-17 19:06:34 +0200

    Updates

---
+++ dist/baseTypes/3.js
@@ -0,0 +1,6 @@
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// str = some;
+// export {};
+//# sourceMappingURL=3.js.map
---
+++ src/generic/4.ts
@@ -0,0 +1,15 @@
+// /*
+//   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+// */
+
+// class Component {
+//   constructor(public props: T) {}
+// }
+
+// class Page extends Component {
+//   pageInfo() {
+//     console.log(this.props.title);
+//   }
+// }
+
+// export {};
